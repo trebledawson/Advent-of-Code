f72e0e6fb4f20bc7321236a056f18bbc9058e38f
Glenn Dawson <trebledawson@gmail.com>
2023-12-07 14:50:42 -0500

    Update day5.py
    Optimized some set arithmetic. Removed some redundant off-by-one subtraction checks.

--- 2023/day5.py
+++ 2023/day5.py
@@ -5,6 +5,7 @@ def main():
     start = timer()
     part_one()
     print(f'Time: {timer() - start} seconds.')
+    
     start = timer()
     part_two()
     print(f'Time: {timer() - start} seconds.')
@@ -31,38 +32,39 @@ def part_two():
     seeds, maps = parse_almanac(filename)
     
     # First, get candidate seeds by running backward through the maps
-    candidate_seeds = {0}
+    candidate_seeds = {0}  # Start with lower bound of 0. Can think of as "location-to-location" map [0, inf)
     for map_type in reversed(maps):
         new_seeds = set()  # For storing candidates from this map
-        low = float('inf')
-        high = float('-inf')
+        high = 0  # This is to find the lower bound of the highest range, [high, inf)
         
-        found = []
         for map_range in maps[map_type]:
+            found = set()  # Store set of found seeds to find those not modified by map
             for seed in candidate_seeds:
                 # Inverse image of output boundaries. Should only occur once per output
-                if maps[map_type][map_range][0] <= seed <= maps[map_type][map_range][1]:
+                if maps[map_type][map_range][0] <= seed < maps[map_type][map_range][1]:
                     new_seeds.add(seed - maps[map_type][map_range][2])  # Map is source + diff = destination
-                    found.append(seed)
+                    found.add(seed)
+            candidate_seeds -= found  # Remove found seeds from candidates
             
-            # Get new boundaries
+            # Check if new highest lower bound
             if map_range[1] + 1 > high:
                 high = map_range[1] + 1
-            new_seeds.update({map_range[0], high})
+                
+            new_seeds.add(map_range[0])  # Add lower bounds of all given ranges
+        new_seeds.add(high)  # Add lower bound of highest range, i.e. [high, inf)
         
-        # Seeds not found in non-idempotent mapping map to themselves
-        new_seeds.update({seed for seed in candidate_seeds if seed not in found})
+        # Seeds not found in non-idempotent ranges map to themselves
+        new_seeds.update(candidate_seeds)
         
-        # Throw out the old candidates and replace with the new ones
-        candidate_seeds = new_seeds
+        candidate_seeds = new_seeds  # Replace candidates
         
-    # Now, find the candidate seeds that lie in the seed ranges
-    test_seeds = set()
+    # Now, find the candidate seeds that lie in the seed ranges, including seed range lower bounds
+    test_seeds = {seeds[0]}
     while len(seeds) > 0:    
-        seeds_in_range = {seed for seed in candidate_seeds if seeds[0] <= seed <= seeds[0] + seeds[1] - 1}
+        seeds_in_range = {seed for seed in candidate_seeds if seeds[0] <= seed < seeds[0] + seeds[1]}
         test_seeds.update(seeds_in_range)
-        test_seeds.update({seeds[0], seeds[0] + seeds[1] - 1})
         candidate_seeds -= seeds_in_range
+        test_seeds.add(seeds[0])
         seeds = seeds[2:]     
     
     # Finally, pass the test seeds through the maps to find the smallest location
@@ -79,7 +81,7 @@ def parse_almanac(filename):
             if i > 0:
                 if line[0].isdigit():  # Create map entry. Cannot fire before new_map is created
                     dest, src, rng = [int(n.strip()) for n in line.split(' ')]
-                    new_map[(src, src + rng - 1)] = (dest, dest + rng - 1, dest - src)
+                    new_map[(src, src + rng)] = (dest, dest + rng, dest - src)
                 elif len(line) > 1:  # Set current map if line is not empty
                     current_map = line[:-6]   
                     new_map = dict()            
@@ -93,14 +95,16 @@ def parse_almanac(filename):
 
 
 def pass_through_maps(seeds, maps):
-    for i, seed in enumerate(seeds):
+    location = float('inf')
+    for seed in seeds:
         for map_type in maps:
             for map_range in maps[map_type]:
-                if map_range[0] <= seed <= map_range[1]:
-                    seed = maps[map_type][map_range][0] + seed - map_range[0]
+                if map_range[0] <= seed < map_range[1]:
+                    seed = maps[map_type][map_range][0] - map_range[0] + seed
                     break
-        location = seed if i == 0 else min(location, seed)
-        
+        if seed < location:
+            location = seed
+            
     return location
 
 
